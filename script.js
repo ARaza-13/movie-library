bf9da9dff96bd211c4ab761bc5fdb440fc1297e7
Ali Raza <aliraza021419@gmail.com>
2023-06-12 14:39:44 -0400

    Movie form functionality creates new movie Object on submission and displays the movie Object on the page

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const library = document.querySelector('.library');
+const movieForm = document.getElementById('submitMovie');
+
+movieForm.onsubmit = addMovie;
 
 let myLibrary = [];
 
@@ -9,44 +12,45 @@ function Movie(title, runtime, year, rating) {
     this.rating = rating;
 }
 
-function addMovietoLibrary(movie) {
-    myLibrary.push(movie);
-    return myLibrary;
+function getMovieInput() {
+    const title = document.getElementById('title').value;
+    const runtime = document.getElementById('runtime').value;
+    const year = document.getElementById('year').value;
+    const rating = document.getElementById('rating').value;
+    return new Movie(title, runtime, year, rating);
+    // const movieInfo = Array.from(document.querySelectorAll('#submitMovie input')).reduce((acc, input) => ({
+    //      ...acc, [input.id]: input.value 
+    // }), {});
 }
 
-function displayMovie() {
-    for (const movie of myLibrary) {
-        let card = document.createElement('div');
-        card.classList.add('card');
-        let title = document.createElement('div');
-        title.classList.add('title');
-        let runtime = document.createElement('div');
-        runtime.classList.add('runtime');
-        let year = document.createElement('div');
-        year.classList.add('year');
-        let rating = document.createElement('div');
-        rating.classList.add('rating');
-
-        title.textContent = `Title: ${movie.title}`;
-        runtime.textContent = `Runtime: ${movie.runtime}`;
-        year.textContent = `Year: ${movie.year}`;
-        rating.textContent = `Rating: ${movie.rating}`;
-
-        card.appendChild(title);
-        card.appendChild(runtime);
-        card.appendChild(year);
-        card.appendChild(rating);
-        library.appendChild(card);
-    }
+function addMovie(e) {
+    e.preventDefault();
+    const newMovie = getMovieInput();
+    myLibrary.push(newMovie);
+    console.log(myLibrary);
+    displayMovie(newMovie);
 }
 
-const movie1 = new Movie('The Dark Knight', '152 mins', '2008', 5);
-const movie2 = new Movie('Spider-Man: Into The Spider-Verse', '117 mins', '2018', 5);
-const movie3 = new Movie('The Batman', '177 mins', '2022', 5);
+function displayMovie(movie) {
+    let movieCard = document.createElement('div');
+    movieCard.classList.add('card');
+    let title = document.createElement('div');
+    title.classList.add('title');
+    let runtime = document.createElement('div');
+    runtime.classList.add('runtime');
+    let year = document.createElement('div');
+    year.classList.add('year');
+    let rating = document.createElement('div');
+    rating.classList.add('rating');
 
-addMovietoLibrary(movie1);
-addMovietoLibrary(movie2);
-addMovietoLibrary(movie3);
+    title.textContent = `Title: ${movie.title}`;
+    runtime.textContent = `Runtime: ${movie.runtime}`;
+    year.textContent = `Year: ${movie.year}`;
+    rating.textContent = `Rating: ${movie.rating}`;
 
-console.log(myLibrary);
-displayMovie();
+    movieCard.appendChild(title);
+    movieCard.appendChild(runtime);
+    movieCard.appendChild(year);
+    movieCard.appendChild(rating);
+    library.appendChild(movieCard); 
+}
