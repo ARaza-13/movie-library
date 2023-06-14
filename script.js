a4f02fc5c36ea749cc0e505ab9c3c3e75ef1f981
Ali Raza <aliraza021419@gmail.com>
2023-06-14 19:59:15 -0400

    Added background colors and styled header elements

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const form = document.getElementById('submitMovie');
 const openMovieForm = document.querySelector('.add');
 const closeMovieForm = document.querySelector('.cancel');
 const errorMsg = document.querySelector('.error-msg');
+const maxYear = document.getElementById('year');
 
+let currentYear = new Date().getFullYear();
+maxYear.max = currentYear;
 // library array to store all our movie objects
 let myLibrary = [];
 
@@ -25,20 +28,20 @@ function closeForm() {
 }
 
 // create movie object
-function Movie(title, runtime, year, rating) {
+function Movie(title, year, runtime, rating) {
     this.title = title;
-    this.runtime = runtime;
     this.year = year;
+    this.runtime = runtime;
     this.rating = rating;
 }
 
 // create a new movie object based on user input 
 function getMovieInput() {
     const title = document.getElementById('title').value;
-    const runtime = document.getElementById('runtime').value;
     const year = document.getElementById('year').value;
+    const runtime = document.getElementById('runtime').value;
     const rating = document.getElementById('rating').value;
-    return new Movie(title, runtime, year, rating);
+    return new Movie(title, year, runtime, rating);
 }
 
 // check if movie is already in library
@@ -67,27 +70,27 @@ function addMovie(e) {
 function displayMovie(movie) {
     let movieCard = document.createElement('div');
     let title = document.createElement('div');
-    let runtime = document.createElement('div');
     let year = document.createElement('div');
+    let runtime = document.createElement('div');
     let rating = document.createElement('div');
     let removeBtn = document.createElement('button');
 
     movieCard.classList.add('card');
     title.classList.add('title');
-    runtime.classList.add('runtime');
     year.classList.add('year');
+    runtime.classList.add('runtime');
     rating.classList.add('rating');
     removeBtn.classList.add('remove');
 
     title.textContent = `${movie.title}`;
-    runtime.textContent = `${movie.runtime} mins`;
     year.textContent = `${movie.year}`;
+    runtime.textContent = `${movie.runtime} mins`;
     rating.textContent = `${movie.rating} stars`;
     removeBtn.textContent = 'Remove';
 
     movieCard.appendChild(title);
-    movieCard.appendChild(runtime);
     movieCard.appendChild(year);
+    movieCard.appendChild(runtime);
     movieCard.appendChild(rating);
     movieCard.appendChild(removeBtn);
     library.appendChild(movieCard); 
