a1240328da5dc08653b9cc384435f034950e7f5d
Ali Raza <aliraza021419@gmail.com>
2023-07-17 17:02:28 -0400

    Added placeholder image for poster if input is not provided, added overlay for form popup, and added the current year for the max year input

--- script.js
+++ script.js
@@ -25,12 +25,8 @@ class Library {
         this.movies.push(newMovie);
     }
 
-    removeMovie(title) {
-        this.movies = this.movies.filter((movie) => movie.title != title);
-    }
-
-    getMovie(title) {
-        return this.movies.find((movie) => movie.title === title);
+    removeMovie(film) {
+        this.movies = this.movies.filter((movie) => movie != film);
     }
 
     renderMovies() {
@@ -53,8 +49,7 @@ const createMovieCard = (movie) =>  {
 
     const poster = document.createElement('img');
     poster.classList.add('poster');
-    // poster.src = checkPoster(movie.poster);
-    poster.src = `${movie.poster}`;
+    poster.src = checkPoster(movie.poster);
     poster.alt = "poster";
 
     const movieInfo = document.createElement('div');
@@ -93,6 +88,7 @@ const createMovieCard = (movie) =>  {
     return movieCard;
 }
 
+// display stars for the rating input
 const getRating = (rating, movie) => {
     for (let i = 0; i < Math.floor(movie.rating); i++) {
         const star = document.createElement('img');
@@ -107,11 +103,21 @@ const getRating = (rating, movie) => {
     }
 }
 
+// validate if the URL movie image being added is a valid image URL 
+const checkPoster = (poster) => {
+    const validImg = /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(poster);
+    if (validImg) {
+        return poster;
+    } else {
+        return "images/placeholder.jpg";
+    }
+}
+
 const createRemoveBtn = (movie) => {
     const removeBtn = document.createElement('button');
     removeBtn.classList.add('remove');
     removeBtn.classList.add('img-btn');
-    removeBtn.onclick = () => removeMovie(movie.title);
+    removeBtn.onclick = () => removeMovie(movie);
 
     const removeImg = document.createElement('img');
     removeImg.src = "images/close-thick.svg";
@@ -135,27 +141,25 @@ const createEditButton = (movie) => {
     return editBtn;
 }
 
+const overlay = document.getElementById('overlay');
 const addMovieForm = document.getElementById('add-movie-form');
 const addMovieModal = document.getElementById('add-movie-modal');
-
 const editMovieForm = document.getElementById('edit-movie-form');
 const editMovieModal = document.getElementById('edit-movie-modal');
-
 const addMovieBtn = document.getElementById('add-movie-btn');
 const closeBtn = document.querySelectorAll('.cancel');
+const addMaxYear = document.getElementById('add-year');
+const editMaxYear = document.getElementById('edit-year');
 
 const openAddMovieModal = () => {
     addMovieForm.reset();
     addMovieModal.classList.add('active');
-}
-
-const closeModal = () => {
-    addMovieModal.classList.remove('active');
-    editMovieModal.classList.remove('active');
+    overlay.classList.add('active');
 }
 
 const openEditMovieModal = (movie) => {
     editMovieModal.classList.add('active');
+    overlay.classList.add('active');
 
     // populate the edit form with movie info
     const editInputs = getEditInputFields();
@@ -166,6 +170,12 @@ const openEditMovieModal = (movie) => {
     editInputs.posterInput.value = movie.poster;
 
     editMovieForm.onsubmit = (e) => updateMovie(e, movie);
+}
+
+const closeModal = () => {
+    addMovieModal.classList.remove('active');
+    editMovieModal.classList.remove('active');
+    overlay.classList.remove('active');
 }
 
 const getMovieInput = () => {
@@ -199,8 +209,8 @@ const addMovie = (e) => {
     closeModal();
 }
 
-const removeMovie = (title) => {
-    library.removeMovie(title);
+const removeMovie = (movie) => {
+    library.removeMovie(movie);
     library.renderMovies();
     console.log(library.movies);
 }
@@ -224,150 +234,10 @@ const updateMovie = (e, movie) => {
 addMovieBtn.onclick = openAddMovieModal;
 addMovieForm.onsubmit = addMovie;
 closeBtn.forEach(modal => modal.onclick = closeModal);
+const currentYear = new Date().getFullYear();
+addMaxYear.max = currentYear;
+editMaxYear.max = currentYear;
 const movie = new Movie("The Dark Knight", "2008", "152", "5", "");
 library.addMovie(movie);
 library.renderMovies();
 console.log(library.movies);
-
-// const addformWrap = document.querySelector('.add-movie');
-// const editformWrap = document.querySelector('.edit-movie');
-// const addMovieForm = document.getElementById('add-movie');
-// const editMovieForm = document.getElementById('edit-movie');
-// const openMovieForm = document.querySelector('.add');
-// const closeMovieForm = document.querySelectorAll('.cancel');
-// const errorMsg = document.querySelector('.error-msg');
-// const maxYear = document.getElementById('add-year');
-
-// let currentYear = new Date().getFullYear();
-// maxYear.max = currentYear;
-// // library array to store all our movie objects
-// let myLibrary = [];
-
-// addMovieForm.onsubmit = addMovie;
-// // add and close button to hide/show movie form
-// openMovieForm.onclick = displayForm;
-// closeMovieForm.forEach(close => close.onclick = closeForm);
-
-// function displayForm(e) {
-//     if (e.target.classList.contains('add')) {
-//         addformWrap.classList.add('active');
-//     } else {
-//         editformWrap.classList.add('active');
-//     }
-// }
-
-// function closeForm() {
-//     addformWrap.classList.remove('active');
-//     addMovieForm.reset();
-//     editformWrap.classList.remove('active');
-//     // editMovieForm.reset();
-//     errorMsg.classList.remove('active');
-//     errorMsg.textContent = '';
-// }
-
-// // create movie object
-// function Movie(title, year, runtime, rating, poster) {
-//     this.title = title;
-//     this.year = year;
-//     this.runtime = runtime;
-//     this.rating = rating;
-//     this.poster = poster;
-// }
-
-// // create a new movie object based on user input 
-// function getMovieInput() {
-//     const title = document.getElementById('add-title').value;
-//     const year = document.getElementById('add-year').value;
-//     const runtime = document.getElementById('add-runtime').value;
-//     const rating = document.getElementById('add-rating').value;
-//     const poster = document.getElementById('add-poster').value;
-//     return new Movie(title, year, runtime, rating, poster);
-// }
-
-// function getEditedMovie() {
-//     const editTitle = document.getElementById('edit-title').value;
-//     const editYear = document.getElementById('edit-year').value;
-//     const editRuntime = document.getElementById('edit-runtime').value;
-//     const editRating = document.getElementById('edit-rating').value;
-//     const editPoster = document.getElementById('edit-poster').value;
-//     return new Movie(editTitle, editYear, editRuntime, editRating, editPoster);
-// }
-
-// // check if movie is already in library
-// function validateInput(input) {
-//     if (myLibrary.some(movie => movie.title === input.title)) {
-//         errorMsg.textContent = "This movie is already in your library";
-//         errorMsg.classList.add('active');
-//         return;
-//     }
-//     return true;
-// }
-
-// // validate if the URL movie image being added is a valid image URL 
-// function checkPoster(poster) {
-//     let validImg = /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(poster);
-//     if (validImg) {
-//         return poster;
-//     } else {
-//         return "images/placeholder.jpg";
-//     }
-// }
-
-// // add the new movie object (acquired from input) into our library array
-// function addMovie(e) {
-//     e.preventDefault();
-//     const newMovie = getMovieInput();
-//     if (validateInput(newMovie)) {
-//         myLibrary.push(newMovie);
-//         console.log(myLibrary);
-//         closeForm();
-//         updateMovieGrid();
-//     }
-// }
-
-// // remove the movie object from our library array and from the webpage display
-// function removeMovie(e) {
-//     const movie = getMovie(e.target);
-//     console.log(myLibrary);
-//     const index = myLibrary.indexOf(movie);
-//     myLibrary.splice(index, 1);
-//     updateMovieGrid();
-//     console.log(myLibrary);
-// }
-
-// // edit the movie object from library array and display it on webpage
-// function editMovie(e) {
-//     const editTitle = document.getElementById('edit-title');
-//     const editYear = document.getElementById('edit-year');
-//     const editRuntime = document.getElementById('edit-runtime');
-//     const editRating = document.getElementById('edit-rating');
-//     const editPoster = document.getElementById('edit-poster');
-
-//     const movie = getMovie(e.target);
-//     const index = myLibrary.indexOf(movie);
-//     console.log(movie);
-
-//     editTitle.value = movie.title;
-//     editYear.value = movie.year;
-//     editRuntime.value = movie.runtime;
-//     editRating.value = movie.rating;
-//     editPoster.value = movie.poster;
-
-//     editMovieForm.onsubmit = (e) => updateMovie(e, index);
-// }
-
-// function updateMovie(e, index) {
-//     e.preventDefault();
-//     const newMovie = getEditedMovie();
-//     console.log(newMovie);
-//     myLibrary.splice(index, 1, newMovie);
-//     console.log(myLibrary);
-//     closeForm();
-//     updateMovieGrid();
-// }
-
-// function getMovie(current) {
-//     const title = current.parentNode.querySelector('.title').textContent;
-//     return myLibrary.find(movie => movie.title === title);
-
-// }
